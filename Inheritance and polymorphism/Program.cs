62419ea3153cdb532c6b2e558a0bf3dd1c6b494a
Denys Shaitan <schaitan.den@gmail.com>
2025-09-27 19:36:26 +0200

    Make first code

--- Inheritance and polymorphism/Program.cs
+++ Inheritance and polymorphism/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Inheritance_and_polymorphism
 {
-    internal class Program
+
+    class BaseClass
+    {
+        public  virtual void Method()
+        {
+            Console.WriteLine("Method from BaseClass");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public override void Method()
+        {
+            base.Method();
+
+            Console.WriteLine("Method from DerivedClass");
+        }
+    }
+    
+
+     
+     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DerivedClass instance = new DerivedClass();
+            instance.Method();
+
+            
+           
         }
     }
 }
